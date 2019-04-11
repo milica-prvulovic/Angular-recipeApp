3216d2f3a74873594417eb4f9b724be9b7b7ef17
milica-prvulovic <milica.prvulovic90@gmail.com>
2019-04-11 11:40:36 +0200

    Editing thse Shopping-List via NgRx 2

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,7 +3,6 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { SharedModule } from '../shared/shared.module';
 import { AppRoutingModule } from '../app-routing.module';
-import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { RecipeService } from '../recipes/recipe.service';
 import { DataStorageService } from '../shared/data-storage.service';
 import { AuthService } from '../auth/auth.service';
@@ -26,7 +25,6 @@ import { LoggingInterceptor } from '../shared/logging.interceptor';
         HeaderComponent
     ],
     providers: [
-        ShoppingListService,
         RecipeService, 
         DataStorageService, 
         AuthService,
