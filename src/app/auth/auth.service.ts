798067c33b92e05c0444d7f657fab4b66f1beff2
milica-prvulovic <milica.prvulovic90@gmail.com>
2019-04-11 15:31:15 +0200

    Dispatch Actions

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,15 +1,21 @@
 import * as firebase from 'firebase';
 import { Router } from '@angular/router';
 import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import * as fromApp from '../store/app.reducers';
+import * as AuthAction from './store/auth.actions';
 
 @Injectable()
 export class AuthService {
-    token: string;
-
-    constructor(private router: Router) {}
+    constructor(private router: Router, private store: Store<fromApp.AppState>) {}
 
     signupUser(email: string, password: string) {
         firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then(
+            user => {
+                this.store.dispatch(new AuthAction.Signup());
+            }
+        )
         .catch (
             error => console.log(error) 
         )
@@ -19,10 +25,13 @@ export class AuthService {
         firebase.auth().signInWithEmailAndPassword(email, password)
         .then(
             response => {
+                this.store.dispatch(new AuthAction.Signin());
                 this.router.navigate(['/']);
                 firebase.auth().currentUser.getIdToken()
                     .then(
-                        (token: string) => this.token = token
+                        (token: string) => {
+                            this.store.dispatch(new AuthAction.SetToken(token));
+                        }
                     )
             }
         )
@@ -34,20 +43,7 @@ export class AuthService {
 
     logout() {
         firebase.auth().signOut();
-        this.token = null;
-    }
-
-    getToken() {
-        firebase.auth().currentUser.getIdToken()
-         .then(
-                (token: string) => this.token = token
-            );
-
-        return this.token; 
-    }
-
-    isAuthenticated() {
-        return this.token != null;
+        this.store.dispatch(new AuthAction.Logout());
     }
 
 }
