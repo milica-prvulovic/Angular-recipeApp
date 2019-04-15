29bc6a27ac8b27571a2ee32fcd5896b458dbfb5f
milica-prvulovic <milica.prvulovic90@gmail.com>
2019-04-15 16:41:39 +0200

    Only React to Actions Once via take(1)

--- src/app/shared/auth.interceptor.ts
+++ src/app/shared/auth.interceptor.ts
@@ -5,6 +5,7 @@ import { Store } from '@ngrx/store';
 import * as formApp from '../store/app.reducers';
 import * as fromAuth from '../auth/store/auth.reducers';
 import 'rxjs/add/operator/switchMap';
+import { take } from 'rxjs/operators';
 
 @Injectable ()
 export class AuthInterceptor implements HttpInterceptor {
@@ -14,11 +15,11 @@ export class AuthInterceptor implements HttpInterceptor {
         console.log('Intercepted!', req);
         // const copiedReq = req.clone({headers: req.headers.set('','')});
         return this.store.select('auth')
+        .pipe(take(1))
         .switchMap((authState: fromAuth.State) => {
             const copiedReq = req.clone({params: req.params.set('auth',authState.token)});
             return next.handle(copiedReq);
         })
-      
        // return null;
     }
 }
