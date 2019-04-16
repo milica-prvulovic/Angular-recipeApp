162db66db2971de0fa75d9944fe19027274718ab
milica-prvulovic <milica.prvulovic90@gmail.com>
2019-04-16 10:01:44 +0200

    Effects 2 and Adding Auth Signup

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -21,6 +21,6 @@ export class SignupComponent implements OnInit {
   onSingup(form: NgForm) {
     const email = form.value.email;
     const password = form.value.password;
-    this.store.dispatch(new AuthActions.TrySignup{username: email, password: password});
+    this.store.dispatch(new AuthActions.TrySignup({username: email, password: password}));
   }
 }
--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -3,11 +3,38 @@ import { Injectable } from '@angular/core';
 
 import * as AuthActions from './auth.actions';
 
+import 'rxjs/add/operator/map';
+import {mergeMap} from 'rxjs/operators';
+
+import { fromPromise} from 'rxjs/observable/fromPromise';
+import * as firebase from 'firebase';
+
 @Injectable()
 export class AuthEffects {
     @Effect()
     authSignup = this.actions$
-    .pipe(ofType(AuthActions.TRY_SIGNUP));
+    .pipe(ofType(AuthActions.TRY_SIGNUP))
+    .map((action: AuthActions.TrySignup) => {
+        return action.payload;
+    })
+    .switchMap((authData: {username: string, password: string}) => {
+        return fromPromise(firebase.auth().createUserWithEmailAndPassword(
+            authData.username , authData.password));
+    })
+    .switchMap(() => {
+        return fromPromise(firebase.auth().currentUser.getIdToken());
+    })
+    .pipe(mergeMap((token: string) => {
+        return [
+            {
+                type: AuthActions.SIGNUP
+            },
+            {
+                type: AuthActions.SET_TOKEN,
+                payload: token
+            }
+        ];
+    }));
 
     constructor( private actions$: Actions) {
     }
