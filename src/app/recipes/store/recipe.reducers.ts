0daf17ac0b7c6968692c5a3faa143fb7c8f0023a
milica-prvulovic <milica.prvulovic90@gmail.com>
2019-04-16 14:36:51 +0200

    Adding Recipe Reducers

--- src/app/recipes/store/recipe.reducers.ts
+++ src/app/recipes/store/recipe.reducers.ts
@@ -1,5 +1,6 @@
 import { Recipe } from '../recipe.model';
 import { Ingredient } from 'src/app/shared/ingredient.model';
+import * as RecipeActions from './recipe.actions';
 
 export interface FeaturState {
     recipes: State
@@ -39,6 +40,38 @@ const initialState: State = {
       ]
 };
 
-export function recipeReducer(state = initialState, action) {
-    return state;
+export function recipeReducer(state = initialState, action: RecipeActions.RecipeActions) {
+    switch (action.type) {
+        case (RecipeActions.SET_RECIPES) :
+            return {
+                ...state,
+                recipes: [...action.payload]
+            };
+        case (RecipeActions.ADD_RECIPE) :
+            return {
+                ...state,
+                recipes: [...state.recipes, action.payload]
+            };
+        case (RecipeActions.UPDATE_RECIPE) :
+            const recipe = state.recipes[action.payload.index];
+            const updateRecipe = {
+                ...recipe,
+                ...action.payload.updateRecipe
+            };
+            const recipes = [...state.recipes];
+            recipes[action.payload.index] = updateRecipe;
+            return {
+               ...state,
+               recipes: recipes 
+            };
+        case (RecipeActions.DELETE_RECIPE):
+            const oldRecipes = [...state.recipes];
+            oldRecipes.splice(action.payload, 1);
+            return {
+                ...state,
+                recipes: oldRecipes
+            };
+        default: 
+            return state;
+    }
 }
